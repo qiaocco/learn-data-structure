9b1e34d9c96a50a5bc87b05891aabd4f46fdd376
qiaocc <qiaocco@gmail.com>
2022-11-18 16:37:44 +0800

    344

---
+++ 04-双指针/[344]反转字符串.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=344 lang=python3
+#
+# [344] 反转字符串
+#
+
+# 时间复杂度： O(n)
+# 空间复杂度: O(1)
+
+# @lc code=start
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0 , len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+# @lc code=end
+
